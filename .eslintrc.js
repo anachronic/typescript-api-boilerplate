de4e6ea4594659dcde2620a6341853b99f74501d
Nicolás Salas <nicolas.salas@imfd.cl>
2021-02-07 23:32:57 -0300

    Set up promises linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,15 @@ module.exports = {
         "@typescript-eslint/no-var-requires": "off",
       },
     },
+    {
+      files: "**/*.ts",
+      parserOptions: {
+        project: ["tsconfig.json"],
+      },
+      rules: {
+        "@typescript-eslint/await-thenable": "error",
+        "@typescript-eslint/no-floating-promises": "error",
+      },
+    },
   ],
 };
