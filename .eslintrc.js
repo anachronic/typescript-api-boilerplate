fa670bcbb8e0bb68576a0cafa9085b2b2a56a901
Nicolás Salas <nicolas.salas@imfd.cl>
2021-02-11 05:38:42 -0300

    Update ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,13 +20,18 @@ module.exports = {
       },
     },
     {
-      files: "**/*.ts",
+      files: "src/**/*.ts",
       parserOptions: {
         project: ["tsconfig.json"],
       },
       rules: {
         "@typescript-eslint/await-thenable": "error",
         "@typescript-eslint/no-floating-promises": "error",
+      },
+    },
+    {
+      files: "**/*.ts",
+      rules: {
         "@typescript-eslint/no-explicit-any": "off",
       },
     },
