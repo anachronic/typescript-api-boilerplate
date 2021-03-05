a2c99309b7e574c1f81a6c877d7bbe4e90cb20a3
Nicolás Salas <nicolas.salas@imfd.cl>
2021-03-05 00:33:40 -0300

    Use a default-like error handler
    This allows us to write middleware that catches specific exceptions

--- src/middleware/errors.ts
+++ src/middleware/errors.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express'
-import { ExpressErrorMiddlewareInterface as ErrorMiddleware, Middleware } from 'routing-controllers'
+import {
+  ExpressErrorMiddlewareInterface as ErrorMiddleware,
+  HttpError,
+  Middleware,
+} from 'routing-controllers'
 import { InjectValue } from 'typescript-ioc'
 import { Logger } from 'winston'
 
@@ -8,20 +12,44 @@ export class ErrorHandlingMiddleware implements ErrorMiddleware {
   @InjectValue('logger')
   private logger: Logger
 
-  error(err: Error, req: Request, res: Response): void {
-    const stackList = err.stack?.split('\n')
-    this.logger.error(stackList || [])
+  private processJsonError(error: any) {
+    if (typeof error.toJSON === 'function') return error.toJSON()
 
-    res.status(500)
-    const response: Record<string, any> = {
-      message: err.message,
-      name: err.name,
+    const processedError: any = {}
+    if (error instanceof Error) {
+      const name = error.name && error.name !== 'Error' ? error.name : error.constructor.name
+      processedError.name = name
+
+      if (error.message) processedError.message = error.message
+      if (error.stack && process.env.NODE_ENV !== 'production')
+        processedError.stack = error.stack.split('\n')
+
+      Object.keys(error)
+        .filter(
+          (key) =>
+            key !== 'stack' &&
+            key !== 'name' &&
+            key !== 'message' &&
+            (!(error instanceof HttpError) || key !== 'httpCode')
+        )
+        .forEach((key) => (processedError[key] = (error as any)[key]))
+
+      return Object.keys(processedError).length > 0 ? processedError : undefined
     }
 
-    if (process.env.NODE_ENV !== 'production') {
-      response['trace'] = stackList || []
+    return error
+  }
+
+  error(error: any, request: Request, response: Response): void {
+    if (error.httpCode) {
+      response.status(error.httpCode)
+    } else {
+      response.status(500)
     }
 
-    res.json(response)
+    // send error content
+    response.json(this.processJsonError(error))
+
+    throw error
   }
 }
