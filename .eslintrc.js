f61375506f97d8bda0113f9dba57f51fe0222102
Nicolás Salas <nicolas.salas@imfd.cl>
2021-03-09 23:42:45 -0300

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
       rules: {
         '@typescript-eslint/await-thenable': 'error',
         '@typescript-eslint/no-floating-promises': 'error',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
       },
     },
     {
