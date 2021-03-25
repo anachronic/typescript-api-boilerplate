b975497d630831f82406b727caed68ddf8c3b3d2
Nicolás Salas <nicolas.salas@imfd.cl>
2021-03-25 00:54:25 -0300

    Use pino to log whether the server has started

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import 'source-map-support/register'
 
 import { app } from './app'
+import { getLogger } from './logger'
+
+const logger = getLogger('server')
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log('started')
+  logger.info('started')
 })
